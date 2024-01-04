02a772f253554a582b872889a83f46f570da4dd7
David_Karnik <david.karnik@tul.cz>
2024-01-04 11:23:02 +0100

    Add WebSecur config and allow CORS

--- backend/src/main/java/service/backend/controller/userInfoController.java
+++ backend/src/main/java/service/backend/controller/userInfoController.java
@@ -1,5 +1,29 @@
 package service.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api") // prefix
 public class userInfoController {
-    
+    @GetMapping("/home")
+    public String home() {
+
+        return "";
+    }
+
+    @GetMapping("/getName")
+    public ResponseEntity<Map<String, String>> getName() {
+        Map<String, String> response = new HashMap<>();
+        response.put("name", "John Doe"); // Nahraďte tím, co odpovídá vašim datům
+
+        return ResponseEntity.ok(response);
+    }
 }
---
+++ backend/src/main/java/service/backend/security/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package service.backend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")  // Povolit specifické domény
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
