79ea3002158a4be736ce27c638cd33aaa44b5c4d
David_Karnik <david.karnik@tul.cz>
2024-01-21 15:53:47 +0100

    Refactor

--- backend/src/main/java/service/backend/controller/AccountController.java
+++ backend/src/main/java/service/backend/controller/AccountController.java
@@ -1,6 +1,5 @@
 package service.backend.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.backend.model.Balance;
